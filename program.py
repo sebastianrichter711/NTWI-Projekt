9cfbebee9b68995a4b5c05dcb8ade6e90ff8e4e0
sebix354 <sebarokita733@gmail.com>
2022-05-23 17:20:57 +0200

    Sprzątanie w kodzie

--- program.py
+++ program.py
@@ -3,6 +3,19 @@ from cmath import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Granulacja linii łamanej
+# Nowe Trendy w Informatyce - projekt
+# Piotr SOROCIAK, Sebastian RICHTER
+# Informatyka, sem. 1 gr. OS SSM
+
+# Opis projektu
+# Linia łamana L zbudowana jest z n - 1 odcinków pomiędzy n punktami
+# L = ((x1; y1), (x2; y2), ... , (xi; yi), ... , (xn; yx)). Wizualizacja takiej linii nie zawsze
+# jest korzystna, bo nadmiar punktów może mocno zaciemnić obraz. Dlatego
+# wizualizowane będą tylko niektóre punkty. Trzeba to zrobić tak, żeby błąd pomiędzy
+# oryginalną linią łamaną a przybliżeniem był jak najmniejszy. Algorytm
+# przybliżania jest koncepcyjnie znany. W projekcie wymagana jest wizualizacja
+# 2D aproksymacji linii łamanej.
 
 # points = np.array([
 #         [-11, 8],
@@ -47,25 +60,41 @@ import numpy as np
 # ])
 
 points = np.array([
-        [5, 5],
-        [5, 4],
-        [5.1, 3],
-        [5, 2],
-        [5.1, 1],
-        [5,-0.5],
-        [4,0.3],
-        [3,-0.2],
-        [2,0.2],
         [1,0],
-        [0, 0.3],
-        [0.1, 1],
-        [0, 2],
-        [-0.1, 3],
-        [0, 4],
-        [-0.1, 5],
+        [2,0.3],
+        [3,1],
+        [4,1.5],
+        [5,2],
+        [6, 3],
+        [5, 5],
+        [4, 3],
+        [5, 2],
+        [6,1.5],
+        [7,1],
+        [8,0.3],
+        [9,0]
 ])
 
-MAX_DISTANCE = 0.3
+# points = np.array([
+#         [5, 5],
+#         [5, 4],
+#         [5.1, 3],
+#         [5, 2],
+#         [5.1, 1],
+#         [5,-0.5],
+#         [4,0.3],
+#         [3,-0.2],
+#         [2,0.2],
+#         [1,0],
+#         [0, 0.3],
+#         [0.1, 1],
+#         [0, 2],
+#         [-0.1, 3],
+#         [0, 4],
+#         [-0.1, 5],
+# ])
+
+MAX_DISTANCE = 0.5
 searched_points = np.empty([0,2])
 
 def find_most_distant_points(points, first_index, last_index):
@@ -85,8 +114,6 @@ def find_most_distant_points(points, first_index, last_index):
         a = ((first_point[1] - last_point[1]) / (first_point[0] - last_point[0]))
     b = -1 #coefficient at y
     c = (first_point[1] - (a*first_point[0]))
-    # print(a)
-    # print(c)
     print("kolejne najdalsze odleglosci w petli z indeksami punktow: ")
     for el in range(first_index, last_index):
         if el != first_index and el != last_index:
@@ -103,7 +130,7 @@ def find_most_distant_points(points, first_index, last_index):
                 max_el_index = el
                 print(max_el_index, max_distance)
     print("info o najdalszym punkcie: ")
-    print (first_index, last_index, max_el_index, max_distance)
+    print(first_index, last_index, max_el_index, max_distance)
     print("\n")
     if max_distance > MAX_DISTANCE:
         most_distant_point = points[max_el_index]
@@ -115,83 +142,45 @@ def find_most_distant_points(points, first_index, last_index):
     return most_distant_points
 
 # Preparing the data to be computed and plotted
-def broken_line_granulation(searched_points,points):
+def broken_line_granulation(searched_points, points):
 
     # Preparing X and y from the given data
     x = points[:, 0].reshape(len(points), 1)
     y = points[:, 1].reshape(len(points), 1)
     
-    sorted_points = points
-    print(sorted_points)
+    print(points)
     print("\n")
     array_size = len(points)
-    # print("Array size:")
-    # print(array_size)
 
-    first_point = sorted_points[0]
-    last_point = sorted_points[array_size-1]
-    # print("Points")
-    # print(first_point)
-    # print(last_point)
-
-    '''
-    #Distance between points
-    x_sum = last_point[0] - first_point[0]
-    y_sum = last_point[1] - first_point[1]
-    pow_x_sum = pow(x_sum,2)
-    pow_y_sum = pow(y_sum,2)
-    distance = sqrt(pow_x_sum + pow_y_sum)
-    print("Distance:")
-    print(distance)
-    '''
+    first_point = points[0]
+    last_point = points[array_size-1]
+    
     searched_points = np.append(searched_points, first_point)
-    searched_points = np.append(searched_points, find_most_distant_points(sorted_points, 0, array_size-1))
+    searched_points = np.append(searched_points, find_most_distant_points(points, 0, array_size-1))
     print("As")
     print(searched_points[0])
     searched_points = np.append(searched_points, last_point)
     array_length = int(len(searched_points)/2)
-    # print("dlugosc tablicy szukanych punktow: ", array_length)
     searched_points = np.reshape(searched_points, (array_length, 2))
+    print(searched_points)
 
-    sorted_searched_points = searched_points
-
-    print(sorted_searched_points)
-
-    sorted_searched_points_array_size = len(sorted_searched_points)
-    # print("Array size:")
-    # print(sorted_searched_points_array_size)
-
+    searched_points_array_size = len(searched_points)
     # Plotting the data points and the best fit line
     plt.scatter(x, y)
 
-    #for i in range(array_size-1):
-    #    x_val = [sorted_points[i][0], sorted_points[i+1][0]]
-    #    y_val = [sorted_points[i][1], sorted_points[i+1][1]]
-    #    plt.plot(x_val,y_val,'y')
-            
-    #x_first_last = [first_point[0], last_point[0]] - to draw a line from a start point to end point 
-    #y_first_last = [first_point[1], last_point[1]]
-    #plt.plot(x_first_last,y_first_last,'b')
-
-    #for k in range(sorted_searched_points_array_size-1):
-    #    x_value = [sorted_searched_points[k][0], sorted_searched_points[k+1][0]]
-    #    y_value = [sorted_searched_points[k][1], sorted_searched_points[k+1][1]]
-    #    plt.plot(x_value,y_value,'r')
-    
     x_values=[]
     y_values=[]
     sorted_x_values=[]
     sorted_y_values=[]
     
     for i in range(array_size):
-        x_values.append(sorted_points[i][0])
-        y_values.append(sorted_points[i][1]) 
+        x_values.append(points[i][0])
+        y_values.append(points[i][1]) 
     
-    for k in range(sorted_searched_points_array_size):
-        sorted_x_values.append(sorted_searched_points[k][0])
-        sorted_y_values.append(sorted_searched_points[k][1])
+    for k in range(searched_points_array_size):
+        sorted_x_values.append(searched_points[k][0])
+        sorted_y_values.append(searched_points[k][1])
     
-        
     plt.title('Wykres granulacji linii łamanej')
     plt.xlabel('X')
     plt.ylabel('Y')
